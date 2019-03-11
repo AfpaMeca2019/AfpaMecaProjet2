52afd4098b447c6798c9b1ce800ed166850e773b
Olivier Bujeaud <35693665+obujeaud@users.noreply.github.com>
2019-03-11 15:45:15 +0100

    Commit changes pom.xml

--- afpanewmeca/afpanewmeca-application/src/main/java/fr/afpa/formation/mecanique/App.java
+++ afpanewmeca/afpanewmeca-application/src/main/java/fr/afpa/formation/mecanique/App.java
@@ -1,9 +1,7 @@
 package fr.afpa.formation.mecanique;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
