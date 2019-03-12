56f623f77dcb11d52b85fcbc7eb573fe9595b91e
Thao Kiab <toonkill@hotmail.com>
2019-03-12 15:08:19 +0100

    init commit

--- afpanewmeca/afpanewmeca-application/src/main/java/fr/afpa/formation/mecanique/controller/ClientController.java
+++ afpanewmeca/afpanewmeca-application/src/main/java/fr/afpa/formation/mecanique/controller/ClientController.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import fr.afpa.formation.mecanique.business.ClientService;
 import fr.afpa.formation.mecanique.persistence.entity.utilisateur.Client;
@@ -27,6 +31,19 @@ public class ClientController {
 		listClient.add(new Client("Gontrand", "Jean-eude", "0605040708", "123@g.com", "CL001", "ABC123", new Date(), true));
 		
 		model.addAttribute("clients", cs.findAll());
+		return "listClient";
+	}
+	
+	@GetMapping("/createClient")
+	public String create(Model model) {
+		model.addAttribute("client", new Client());
+		return "createClient";
+	}
+	
+	@PostMapping("/client/create")
+	//todo
+	public String doCreate(Client client) {
+		cs.create(client);
 		return "listClient";
 	}
 }
