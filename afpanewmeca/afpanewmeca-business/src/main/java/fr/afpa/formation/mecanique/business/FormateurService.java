19b36d9ba06a55efa22e1dbc73bbf6074b859c51
lenlag <44843433+lenlag@users.noreply.github.com>
2019-03-21 16:35:51 +0100

    Update FormateurService.java
    added update + delete methods

--- afpanewmeca/afpanewmeca-business/src/main/java/fr/afpa/formation/mecanique/business/FormateurService.java
+++ afpanewmeca/afpanewmeca-business/src/main/java/fr/afpa/formation/mecanique/business/FormateurService.java
@@ -1,6 +1,7 @@
 package fr.afpa.formation.mecanique.business;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,14 +12,42 @@ import fr.afpa.formation.mecanique.persistence.repository.FormateurRepository;
 @Service
 public class FormateurService {
 	@Autowired
-	FormateurRepository fr;
+	FormateurRepository formateurRepository;
 	
 	public void createFormateur(Formateur f) {
-		fr.save(f);
+		formateurRepository.save(f);
 	}
 	
 	public List<Formateur> findAll(){
-		return (List<Formateur>) fr.findAll();
+		return (List<Formateur>) formateurRepository.findAll();
+	}
+	
+	public Formateur update(Formateur formateur) throws Exception {
+		if ((formateur == null) || (formateur.getId() == null)) {
+			throw new Exception("Le Formateur est null ou son id est null");
+		}
+
+		Long idAChercher = formateur.getId();
+		Optional<Formateur> formateurOptional = formateurRepository.findById(idAChercher);
+
+		if (!formateurOptional.isPresent()) {
+			throw new Exception("Le Formateur n'a pas été trouvé");
+		}
+		return formateurRepository.save(formateur);
+	}
+	
+	
+	public void deleteById(Long id) throws Exception {
+		if (id == null) {
+			throw new Exception("Id est null");
+		}
+
+		Optional<Formateur> formateurOptional = formateurRepository.findById(id);
+		if (!formateurOptional.isPresent()) {
+			throw new Exception("Formateur n'existe pas");
+		}
+
+		formateurRepository.deleteById(id);
 	}
 
 }
